d2994dc3380567764b63a993e6f28da76b6c7f3a
Minseo Park <125115284+FpRaArNkK@users.noreply.github.com>
2025-07-06 16:29:52 +0900

    [Feat] Trainer - Mypage / Edit 위치 분리

---
+++ TnT/Projects/Presentation/Sources/MyPage/Trainer/MyPageEdit/TrainerMyPageEditFeature.swift
@@ -0,0 +1,9 @@
+//
+//  TrainerMyPageEditFeature.swift
+//  Presentation
+//
+//  Created by 박민서 on 7/6/25.
+//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
+//
+
+import Foundation
---
+++ TnT/Projects/Presentation/Sources/MyPage/Trainer/MyPageEdit/TrainerMyPageEditView.swift
@@ -0,0 +1,9 @@
+//
+//  TrainerMyPageEditView.swift
+//  Presentation
+//
+//  Created by 박민서 on 7/6/25.
+//  Copyright © 2025 yapp25thTeamTnT. All rights reserved.
+//
+
+import Foundation
